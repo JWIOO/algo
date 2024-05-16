b8ca500251709f6cc6cee2e37a84236bdbc2e18e
JIWOO <wldn2731@konkuk.ac.kr>
2024-05-16 23:51:03 +0900

    16회차 과제

--- programmers/mode.js
+++ programmers/mode.js
@@ -1,23 +1,23 @@
-
 function solution(array) {
-
-let mode = -1;
-let repeatNum = [];
-let maxRepeatNum = 0;
-
-for (let i = 0; i < array.length; i++) {
-    let num = array[i];
-
-    repeatNum[num] = (repeatNum[num] || 0) + 1;
-
-    if( repeatNum[num] > maxRepeatNum ){
-        mode = num ;
-        maxRepeatNum = repeatNum[num];
-
-    } else if( repeatNum[num] === maxRepeatNum ) {
-        mode = -1;
+    let mode = -1;
+    let repeatNum = [];
+    let maxRepeatNum = 0;
+    let i = 0;
+    
+    while (i < array.length) {
+        let num = array[i];
+        repeatNum[num] = (repeatNum[num] || 0) + 1; 
+    
+        if (repeatNum[num] > maxRepeatNum) {
+            mode = num;
+            maxRepeatNum = repeatNum[num];
+        } else if (repeatNum[num] === maxRepeatNum) {
+            mode = -1;
+        }
+    
+        i++; 
     }
-
-}
-return mode;
-}
+    
+    return mode;
+    }
+g
